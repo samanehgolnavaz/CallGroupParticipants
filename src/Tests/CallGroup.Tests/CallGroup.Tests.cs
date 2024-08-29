3ea9dcdf45a5380f9fc3596e8625d357d6de5790
Samaneh Golanvaz <samanehgolnavaz@gmail.com>
2024-08-29 21:16:30 +0200

    Added Contructore Test

---
+++ src/Tests/CallGroup.Tests/CallGroup.Tests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using FluentAssertions;
+
+
+namespace CallGroup.Tests
+{
+    public class CallGroupTests
+    {
+
+        [Fact]
+        public void Constructor_ThrowsException_WhenParticipantCountIsZero()
+        {
+            Action act = () => new CallGroup<int>(0, _ => Task.CompletedTask, TimeSpan.FromSeconds(1));
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Value 0 should be greater than zero! (Parameter 'participantCount')");
+        }
+        [Fact]
+        public void Constructor_WithValidArguments_ShouldNotThrow()
+        {
+            Action act = () => new CallGroup<int>(1, _ => Task.CompletedTask, TimeSpan.FromSeconds(1));
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Constructor_WithInvalidParticipantCount_ShouldThrow()
+        {
+            Action act = () => new CallGroup<int>(0, _ => Task.CompletedTask, TimeSpan.FromSeconds(1));
+            act.Should().Throw<ArgumentException>();
+        }
+
+    }
+}
